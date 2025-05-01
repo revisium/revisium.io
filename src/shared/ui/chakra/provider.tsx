1af69377f072caec7661296b4f20be2afd2556a1
anton62k <anton.kashirov@gmail.com>
2025-05-01 15:07:21 +0300

    sonar linting

--- src/shared/ui/chakra/provider.tsx
+++ src/shared/ui/chakra/provider.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { system } from 'src/shared/ui/chakra/theme.ts'
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
 
-export function Provider(props: ColorModeProviderProps) {
+export function Provider(props: Readonly<ColorModeProviderProps>) {
   return (
     <ChakraProvider value={system}>
       <ColorModeProvider {...props} />
