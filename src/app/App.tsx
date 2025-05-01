7816a2bc9c6e169a8ec6f20caf8f476c17d1248a
anton62k <anton.kashirov@gmail.com>
2025-05-01 13:51:11 +0300

    init chakra

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,14 @@ import { observer } from 'mobx-react-lite'
 import { FC } from 'react'
 import { ROOT_ROUTES } from 'src/app/config'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Provider } from 'src/shared/ui'
 
 const router = createBrowserRouter(ROOT_ROUTES)
 
 export const App: FC = observer(() => {
-  return <RouterProvider router={router} />
+  return (
+    <Provider>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 })
