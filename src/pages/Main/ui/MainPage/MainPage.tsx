d11f6df9b80bfa0d8ae3d56d525bbf713634d2a1
Anastasia <kashirova.nastya@gmail.com>
2025-05-03 00:46:40 +0300

    add RightContent.tsx

--- src/pages/Main/ui/MainPage/MainPage.tsx
+++ src/pages/Main/ui/MainPage/MainPage.tsx
@@ -1,27 +1,28 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import { IconGit, IconLogo } from 'src/shared/ui/icons'
 import { Button } from 'src/shared/ui/components'
 import { TabContent } from 'src/pages/Main/ui/TabContent/TabContent.tsx'
+import { RightContent } from 'src/pages/Main/ui/RightContent/RightContent.tsx'
 
 export const MainPage = () => {
   return (
     <Flex
-      border="1px solid red"
       position="relative"
       flexDirection="column"
       alignItems="center"
       justifyContent="space-between"
       height="100vh"
+      p={{ base: '32px 0 0 0', md: '32px 0', xl: '24px 0 64px 0' }}
     >
-      <Flex
-        border="1px solid red"
-        w="100%"
-        maxW="1082px"
-        p={{ base: '32px 0 0 0', md: '32px 0', xl: '24px 0 64px 0' }}
-        flexDirection="column"
-        gap="32px"
-      >
-        <Flex justifyContent="space-between" alignItems="center" p={{ base: '16px', md: '16px 32px', xl: '0 24px' }}>
+      <Flex w="100%" maxW="1082px" flexDirection="column" gap="32px">
+        <Flex
+          position="sticky"
+          top={0}
+          bgColor="#FFFFFF"
+          justifyContent="space-between"
+          alignItems="center"
+          p={{ base: '16px', md: '16px 32px', xl: '0 24px' }}
+        >
           <IconLogo />
           <IconGit />
         </Flex>
@@ -29,42 +30,26 @@ export const MainPage = () => {
           flexDirection={{ base: 'column', xl: 'row' }}
           p={{ base: '16px', md: '0 32px', xl: '64px' }}
           gap={{ base: '16px', md: '32px' }}
+          overflowY="auto"
+          css={{
+            '&::-webkit-scrollbar': { display: 'none' },
+            '-ms-overflow-style': 'none',
+            'scrollbar-width': 'none',
+          }}
         >
-          <Flex maxW="600px" flexDirection="column" gap={{ base: '16px', md: '24px', xl: '32px' }} flex={1}>
-            <Text
-              fontSize={{ base: '44px', md: '80px' }}
-              fontStyle={{ base: 'normal' }}
-              fontWeight={{ base: '700' }}
-              lineHeight={{ base: '48px', md: '72px' }}
-            >
-              The central slogan
-            </Text>
-            <Text
-              fontSize={{ base: '13px', md: '16px' }}
-              fontStyle={{ base: 'normal' }}
-              fontWeight={{ base: '400' }}
-              lineHeight={{ base: '17px', md: '21px' }}
-            >
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-              industry's standard text
-            </Text>
-            <Button display={{ base: 'none', md: 'block' }} w={{ base: '100%', md: '328px' }} visual="PrimaryButton">
-              Try Revisium Cloud (Alpha)
-            </Button>
-          </Flex>
-          <Flex flex={1}>
-            <TabContent />
-          </Flex>
+          <RightContent />
+          <TabContent />
         </Flex>
       </Flex>
       <Flex
         position="sticky"
         bottom={0}
         w="100%"
-        h="108px"
+        minH="108px"
         p="0 16px"
         display={{ base: 'flex', md: 'none' }}
         alignItems="center"
+        bgColor="#FFFFFF"
       >
         <Button w="100%" visual="PrimaryButton">
           Try Revisium Cloud (Alpha)
