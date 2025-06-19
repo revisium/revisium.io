06db31e738849e6b7b53c5098d483db773e32ed9
anton62k <anton.kashirov@gmail.com>
2025-06-19 21:36:35 +0300

    feat: video items

--- src/pages/Main/ui/FeatureHighlightsItem/FeatureHighlightsItem.tsx
+++ src/pages/Main/ui/FeatureHighlightsItem/FeatureHighlightsItem.tsx
@@ -1,5 +1,7 @@
-import { Button, Flex, Image, Text } from '@chakra-ui/react'
+import { Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { ASPECT_RATIO } from 'src/pages/Main/config/const.ts'
+import { VideoItem } from 'src/pages/Main/ui/FeatureHighlightsItem/video/VideoItem.tsx'
 
 interface CoreFeaturesItemProps {
   url: string
@@ -12,28 +14,7 @@ export const FeatureHighlightsItem: React.FC<CoreFeaturesItemProps> = ({ url, de
   return (
     <Flex w="100%" maxW={{ xl: '600px' }} direction="column" gap="16px">
       <Flex position="relative" borderRadius="16px" border="1px solid #E2E4E5" bgColor="#F3F3F3" overflow="hidden">
-        <Image src={url} />
-        {isVideo ? (
-          <Button
-            position="absolute"
-            left="4px"
-            bottom="4px"
-            w="24px"
-            h="24px"
-            minW="auto"
-            minH="auto"
-            p="5px"
-            borderRadius="48px"
-            bgColor="#EE7152"
-          >
-            <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M12 6.13405C12.6667 6.51895 12.6667 7.4812 12 7.8661L4.5 12.1962C3.83333 12.5811 3 12.1 3 11.3302L3 2.66995C3 1.90015 3.83333 1.41902 4.5 1.80392L12 6.13405Z"
-                fill="white"
-              />
-            </svg>
-          </Button>
-        ) : undefined}
+        {isVideo ? <VideoItem url={url} /> : <Image src={url} aspectRatio={ASPECT_RATIO} />}
       </Flex>
       <Flex direction="column" gap="8px">
         <Text fontSize={{ base: '16px', md: '18px', xl: '20px' }} fontWeight={700}>
